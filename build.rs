02740e4b085c5a2cd3def1cbbd079c36f90671ec
pbzweihander <pbzweihander@protonmail.com>
2025-07-05 19:34:36 +0900

    Remove anyhow dependency

--- build.rs
+++ build.rs
@@ -1,15 +1,13 @@
 use std::process::Command;
 
-use anyhow::Context;
-
-fn main() -> anyhow::Result<()> {
+fn main() {
     println!("cargo:rerun-if-env-changed=SKIP_TAILWINDCSS");
 
     if std::env::var_os("SKIP_TAILWINDCSS")
         .map(|v| !v.is_empty())
         .unwrap_or(false)
     {
-        return Ok(());
+        return;
     }
 
     println!("cargo:rerun-if-changed=templates");
@@ -18,18 +16,16 @@ fn main() -> anyhow::Result<()> {
     let output = Command::new("tailwindcss")
         .args(["-i", "templates/base.css", "-o", "dist/index.css"])
         .output()
-        .context("failed to run tailwindcss CLI")?;
+        .expect("failed to run tailwindcss CLI");
     if !output.status.success() {
         println!("cargo:warning=tailwindcss stdout:");
         for line in String::from_utf8_lossy(&output.stdout).lines() {
-            println!("cargo:warning={line}");
+            println!("cargo:warning=\t{line}");
         }
         println!("cargo:warning=tailwindcss stderr:");
         for line in String::from_utf8_lossy(&output.stderr).lines() {
-            println!("cargo:warning={line}");
+            println!("cargo:warning=\t{line}");
         }
-        Err(anyhow::anyhow!("tailwindcss CLI failed"))
-    } else {
-        Ok(())
+        panic!("tailwindcss CLI failed")
     }
 }
